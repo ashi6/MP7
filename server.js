2827aabf3ce7594eaabc20045db4d988d055b2c5
ko-tori <ashi6@illinois.edu>
2017-11-30 21:03:20 -0600

    basic framework/working version

--- server.js
+++ server.js
@@ -1,1 +1,51 @@
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
+app.use(express.static("static"));
+
+var room = io.of("/");
+
+var players = {};
+
+var getSpawn = function () {
+	return [Math.floor(Math.random() * 5000) / 10 - 250, Math.floor(Math.random() * 5000) / 10 - 250];
+};
+
+var randomColor = function () {
+	return `hsl(${Math.random() * 360}, 100%, 50%)`;
+};
+
+io.on("connection", function (socket) {
+	console.log(socket.client.id + " joined");
+	var playerData = {
+		id: socket.id,
+		position: getSpawn(),
+		velocity: [0, 0],
+		color: randomColor()
+	};
+	socket.emit("init", playerData);
+
+	for (var id in players) {
+		if (players.hasOwnProperty(id)) {
+			socket.emit("join", players[id]);
+		}
+	}
+
+	players[socket.id] = playerData;
+
+	socket.on("disconnect", function (data) {
+		delete players[socket.client.id];
+		socket.broadcast.emit("leave", socket.client.id);
+	});
+
+	socket.on("update", function(data) {
+		socket.broadcast.emit("update", data);
+	});
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
+    console.log(`Running on port ${port}...`);
+});
--- static/main.js
+++ static/main.js
@@ -1,1 +1,101 @@
+var keyMap = Array(256);
 
+var players = {};
+var player;
+
+var socket;
+
+var windowWidth;
+var windowHeight;
+
+var align = function () {
+	windowWidth = $(window).width();
+	windowHeight = $(window).height();
+	$("#canvas").attr("width", windowWidth)
+		.attr("height", windowHeight);
+};
+
+var update = function () {
+	//player.update();
+	if (keyMap[87] || keyMap[38]) { // W or up arrow key
+		player.position[1] += 10;
+	}
+	if (keyMap[65] || keyMap[37]) { // A or left arrow key
+		player.position[0] -= 10;
+	}
+	if (keyMap[83] || keyMap[40]) { // S or down arrow key
+		player.position[1] -= 10;
+	}
+	if (keyMap[68] || keyMap[39]) { // D or right arrow key
+		player.position[0] += 10;
+	}
+	socket.emit("update", player);
+};
+
+var render = function () {
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+
+	ctx.clearRect(0, 0, windowWidth, windowHeight);
+	ctx.save();
+
+	ctx.translate(windowWidth / 2, windowHeight / 2);
+	ctx.scale(1, -1);
+
+	// Draw other players first
+	for (var id in players) {
+		let player = players[id];
+		ctx.fillStyle = player.color;
+		ctx.fillRect(player.position[0] - 50, player.position[1] - 50, 100, 100);
+	}
+
+	// Draw your player last to it's on top
+	ctx.fillStyle = player.color;
+	ctx.fillRect(player.position[0] - 50, player.position[1] - 50, 100, 100);
+
+	ctx.restore();
+};
+
+var frame = function () {
+	update();
+	render();
+	requestAnimationFrame(frame);
+};
+
+$(window).on("resize", align);
+
+$(document).ready(function () {
+	align();
+
+	var keyPressed = function (e) {
+		var code = (window.event) ? event.keyCode : e.keyCode;
+		keyMap[code] = true;
+	};
+
+	var keyReleased = function (e) {
+		var code = (window.event) ? event.keyCode : e.keyCode;
+		keyMap[code] = false;
+	};
+
+	document.addEventListener('keydown', keyPressed, false);
+	document.addEventListener('keyup', keyReleased, false);
+
+	socket = io.connect(location.pathname);
+
+	socket.on("update", function (data) {
+		players[data.id] = data;
+	});
+
+	socket.on("join", function (data) {
+		players[data.id] = data;
+	});
+
+	socket.on("leave", function (data) {
+		delete players[data];
+	});
+
+	socket.on("init", function (data) {
+		player = data;
+		frame();
+	});
+})
