f7330abec34ab9f7c6e308c2692a54066e50f9b1
Arjun75x <arjun75x@gmail.com>
2017-12-01 10:13:14 -0600

    Player class modified
    Added constructor and instance methods to player class

--- static/Player.js
+++ static/Player.js
@@ -1,3 +1,17 @@
 class Player {
-	
+    constructor(color, position, velocity) {
+        this.color = color;
+        this.position = position;
+        this.velocity = velocity;
+    }
+    isAttacking(player2) {
+        if(this.position == player2.position) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    compareSpeed(player2) {
+        return this.velocity - player2.velocity
+    }
 }
